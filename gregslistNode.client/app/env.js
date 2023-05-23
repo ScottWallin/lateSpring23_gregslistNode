ce958fd1f42f7a49fb3a8c7c8e794fd2b2bcae90
ScottWallin <scott.wallin@me.com>
2023-05-23 16:28:38 -0600

    one day i'll be able to focus

--- gregslistNode.client/app/env.js
+++ gregslistNode.client/app/env.js
@@ -1,7 +1,8 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'dev-m6o4vxqkk7ozrl3g.us.auth0.com'
+export const domain = 'dev-ogvwlqpn7yrkw51n.us.auth0.com'
 export const audience = 'http://sandbox.com'
-export const clientId = 'ZAglb7ROqnSE45QAusJquMfRjQ7JvJvX'
+export const clientId = 'kbUYLZFBDzLHWaydCqitqaHcYiAXMld8'
+
 
